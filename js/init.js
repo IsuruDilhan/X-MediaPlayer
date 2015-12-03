db8487278442ad5233770e500d9e6c7b2b4385f9
IsuruDilhan <isuru.dilhan@yahoo.com>
2015-12-03 19:41:05 +0530

    Initial commit

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Isuru 1 on 28/11/2015.
+ */
+
+new Xplayer({
+    "videoId": "myvid",
+    "captionsOnDefault": false,
+    "seekInterval": 2,
+    "videoTitle": "",
+    "debug": true
+});
+
